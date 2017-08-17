7ac5c016a8bd23f6af78e946b5e9e70269725fe7
Anupom Roy <aroy34@my.centennialcollege.ca>
2017-08-17 11:00:43 -0400

    created an instance of the PickHighestCardForm

--- COMP123-S2017-FinalExam-300853516/Program.cs
+++ COMP123-S2017-FinalExam-300853516/Program.cs
@@ -5,17 +5,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 /*
- * Name:
- * Date:
- * StudentID:
- * Description:
- * Version:
+ * Name: Anupom Roy
+ * Date: August 17,2017
+ * StudentID: 300853516
+ * Description:Pick Highest  card form
+ * Version: 0.1 created an instance of the PickHighestCardForm object
+
  */
 
 namespace COMP123_S2017_FinalExam_300853516
 {
     public static class Program
     {
+        public static PickHighestCardForm pickHighestCardForm;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
---
+++ COMP123-S2017-FinalExam-300853516/SplashForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Anupom Roy
+ * Date: August 17,2017
+ * StudentID: 300853516
+ * Description:Pick Highest  card form using windows forms and UI Controls
+ * Version: 0.1 - Created SplashScreenTimer Tick event
+ */
+namespace COMP123_S2017_FinalExam_300853516
+{
+    public partial class SplashForm : Form
+    {
+        public PickHighestCardForm pickHighestCardForm
+        {
+            get
+            {
+                return Program.pickHighestCardForm;
+            }
+        }
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+            this.pickHighestCardForm.Show();
+        }
+    }
+}
